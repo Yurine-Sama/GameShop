1f31f282baed7f7b0bc332be9afb638c6b2b83ab
Yurine-Sama <Yurine2019@gmail.com>
2022-01-18 14:46:16 +0700

    ver 2.1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const User = require("./models/user.model");
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/register", (req, res) => {
-  res.json({ status: "ok" });
+mongoose.connect(process.env.DATABASE_URL);
+
+app.post("/api/register", async (req, res) => {
   console.log(req.body);
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "Duplicate email" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (user) {
+    return res.json({ status: "ok", user: true });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
 });
 
 app.listen(3001, () => {
